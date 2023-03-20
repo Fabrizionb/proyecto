b592e24838acdb973c3d51022d94adbaad47a744
Fabrizio <100951814+Fabrizionb@users.noreply.github.com>
2023-03-20 22:11:49 +0100

    cambio  mockup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
-import { StudentComponent } from './student/student.component';
-import { FooterComponent } from './footer/footer.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { PageWrapperComponent } from './page-wrapper/page-wrapper.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    SidebarComponent,
     ToolbarComponent,
-    StudentComponent,
-    FooterComponent,
-    NavbarComponent
+    HomeComponent,
+    PageWrapperComponent,
+
   ],
   imports: [
     BrowserModule
