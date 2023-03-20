13fcc9fe31510bb8ac737fdec60b3e9e0c60e112
Fabrizio <100951814+Fabrizionb@users.noreply.github.com>
2023-03-20 17:16:50 +0100

    1 mockup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { StudentComponent } from './student/student.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    StudentComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
